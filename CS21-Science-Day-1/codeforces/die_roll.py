27d7d2ed53dd571711b392be3f39183dcd269e97
AA-HAMZA <ahmed.rk30@gmail.com>
2020-11-20 23:34:08 +0200

    first day, first try

---
+++ CS21-Science-Day-1/codeforces/die_roll.py
@@ -0,0 +1,16 @@
+from fractions import Fraction
+Yakko, Wakko = map(int, input().split())
+
+#Test how many faces can Dot get to win
+Dot = 6-max(Yakko, Wakko)+1
+
+#Printing probablity
+probablity = Fraction(Dot, 6)
+
+if (probablity == 1):
+    print("1/1")
+elif (probablity == 0):
+    print("0/1")
+else:
+    print(probablity)
+
